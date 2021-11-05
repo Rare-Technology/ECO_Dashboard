763145a91ba90b196ff41b6c8e6e7866253ade84
ibidyouadu <angel.d.umana@gmail.com>
2021-11-05 15:57:19 -0400

    update IDN fish data again, includes 2017 data

--- data-raw/fish-surveys.R
+++ data-raw/fish-surveys.R
@@ -101,9 +101,92 @@ fish.surveys <- rbind(fish.surveys, df)
 fish.surveys <- fish.surveys %>% 
   mutate(year = lubridate::year(survey_date))
 
-# MOZ 2020 survey was at the end of the year and about 50 records have a survey date that
+# MOZ 2020 survey was at the end of the year and about 50 records (out of 504) have a survey date that
 # went into January. We will label the year on these as being 2020 as they belong to the
 # 2020 survey
 fish.surveys$year[fish.surveys$country == "MOZ" & fish.surveys$year == 2021] <- 2020
+
+## New IDN dataset
+# Raymond provided an updated dataset that has the 2021 survey as well has historical records for 2019 and 2017
+# This dataset includes the community site name for the survey sites; NTZ and UTZ correspond to Teluk Kolono
+# Teluk Kolono in turn belongs to the LGU Konawe Selatan, which then belongs to SNU South East Sulawesi
+# The 2017 data is made up entirely of Teluk Kolono records.
+# The 2019 records differ from the existing ones by one row; we won't worry about them
+# The 2021 records are almost the same but the count density is more accurate than the ones in fish.surveys
+# since the weight-length formula had to be used in fish.surveys
+# For this stage of data cleaning, we will:
+# 1. Filter the 2021/2017 records from the updated dataset
+# 2. Select appropriate columns from the updated dataset
+# 3. Add level1_name/level2_name columns
+# 4. Drop the 2021 IDN records in fish.surveys
+# 5. Merge the new 2021/2017 records with fish.surveys
+# 6. Update the .rda and document
+
+get_level_name <- function (x, level) {
+  if (x %in% unique(fish.surveys$ma_name)) {
+    fish.surveys %>% 
+      dplyr::filter(ma_name == x) %>% 
+      dplyr::pull(level) %>% 
+      unique()
+  } else if (x == "Teluk Kolono") {
+    switch(level,
+      "level1_name" = "Southeast Sulawesi",
+      "level2_name" = "Konawe Selatan"
+    )
+  }
+}
+get_level_name <- Vectorize(get_level_name)
+
+get_size_class <- function (x) { # need to fix this for existing fish data, its a mess
+  if (x <= 5) {"0-5"}
+  else if (x > 5 & x <= 10) {"5-10"}
+  else if (x > 10 & x <= 20) {"10-20"}
+  else if (x > 20 & x <= 30) {"20-30"}
+  else if (x > 30 & x <= 40) {"30-40"}
+  else if (x > 40 & x <= 50) {"40-50"}
+}
+get_size_class <- Vectorize(get_size_class)
+
+df <- read.csv("../data/IDN_fish_data.csv", header = TRUE, stringsAsFactors = FALSE)
+df <- df %>%
+  # 1
+  dplyr::filter(Year %in% c(2017, 2021)) %>% 
+  # 2
+  dplyr::select(
+    country = Country,
+    year = Year,
+    month = Month,
+    day = Day,
+    lat = Latitude,
+    lon = Longitude,
+    ma_name = MAR.Name,
+    location_name = Site,
+    location_status = Management.name,
+    transect_no = Transect.number,
+    count = Count,
+    family = Fish.family,
+    species = Fish.taxon,
+    density_ind_ha = density_countha,
+    biomass_kg_ha = Biomass_kgha,
+    length = Size
+  ) %>% 
+  # 3
+  dplyr::mutate(
+    country = "IDN",
+    survey_date = lubridate::ymd(paste(year, month, day, sep = "-")),
+    size_class = as.character(get_size_class(length)),
+    level1_name = get_level_name(ma_name, "level1_name"),
+    level2_name = get_level_name(ma_name, "level2_name")
+  )
+
+fish.surveys <- fish.surveys %>% 
+  # 4
+  dplyr::filter(country != "IDN" | year != 2021) %>% 
+  dplyr::select(intersect(names(df), names(fish.surveys)))
+
+df <- df %>% dplyr::select(names(fish.surveys))
+
+# 5
+fish.surveys <- rbind(fish.surveys, df)
 
 usethis::use_data(fish.surveys, overwrite = TRUE)
