16258a93b6b43dac957616f9904f4393b1c84a8f
ibidyouadu <angel.d.umana@gmail.com>
2023-03-29 18:33:55 -0400

    Update report outputs to include plots

--- R/mod_report.R
+++ R/mod_report.R
@@ -25,7 +25,7 @@ reportServer <- function(id, rv){
       ui_out <- append(ui_out, list(checkboxGroupInput(
         ns("report_metrics"),
         strong("Ecological metrics"),
-        choices = c("Fish Biomass", "Fish Density"),
+        choices = c("Fish biomass", "Fish density"),
         selected = NULL,
         inline = FALSE
       )))
