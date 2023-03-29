e4d7990202dc1f913c9d0a870af00094b39e3037
ibidyouadu <angel.d.umana@gmail.com>
2023-03-29 18:33:13 -0400

    Update plotting code to be more modular so plots can be generated in the plot and reporting tabs

--- R/mod_plot.R
+++ R/mod_plot.R
@@ -47,65 +47,33 @@ plotServer <- function(id, rv){
       }
       data_filtered <- rv$data_filtered
       
-      p <- try(
-        switch(sel_metric,
-          "Fish biomass" = plot_fish_biomass(data_filtered, sel_geom, facet_maa),
-          "Fish density" = plot_fish_density(data_filtered, sel_geom, facet_maa),
-          "Fish diversity" = plot_fish_diversity(data_filtered, sel_geom, facet_maa),
-          "Fish size" = plot_fish_size(data_filtered, sel_geom, facet_maa),
-          "Sapling density" = plot_sapling_tree_density(data_filtered, sel_geom, facet_maa),
-          "Tree diversity" = plot_tree_diversity(data_filtered, sel_geom, facet_maa),
-          "Tree size" = plot_tree_size(data_filtered, sel_geom, facet_maa),
-          "Coral reef cover" = plot_reef_cover(data_filtered, sel_geom, facet_maa),
-          "Coral reef diversity" = plot_reef_diversity(data_filtered, sel_geom, facet_maa),
-          "Seagrass cover" = plot_seagrass_cover(data_filtered, sel_geom, facet_maa),
-          "Seagrass diversity" = plot_seagrass_diversity(data_filtered, sel_geom, facet_maa),
-          "Seagrass height" = plot_seagrass_height(data_filtered, sel_geom, facet_maa),
-          "Oyster density" = plot_oyster_density(data_filtered, sel_geom, facet_maa),
-          "Oyster size" = plot_oyster_size(data_filtered, sel_geom, facet_maa),
-          "Crab density" = plot_crab_density(data_filtered, sel_geom, facet_maa),
-          "Crab size" = plot_crab_size(data_filtered, sel_geom, facet_maa)
-        )
-      )
+      p <- try(PLOT_FUN(sel_metric, data_filtered, sel_geom, facet_maa))
       
       if (class(p) == "try-error") {
         return(div(class="warning_message", tr(rv, PLOT_ERROR)))
-      } else {
-        if (facet_maa) {
-          if (sel_metric == "Coral reef cover") {
-            p$plot <- p$plot + facet_grid(rows = vars(ma_name), cols = vars(location_status))
-            plot_height <- get_plot_height(2*length(rv$sel_maa))
-          } else {
-            p$plot <- p$plot + facet_wrap('ma_name', ncol = 2)
-            plot_height <- get_plot_height(length(rv$sel_maa))
-          }
-        } else {
-          if (sel_metric == "Coral reef cover") {
-            p$plot <- p$plot + facet_wrap("location_status")
-          }
-          plot_height <- 'auto'
-        }
-        p$plot$facet$params$free$y <- !y_scale
-        p$plot <- p$plot + ggplot2::labs(caption = WATERMARK_LABEL)
-        rv$current_plot <- p$plot
-        rv$current_plot_height <- plot_height
-        rv$current_plot_data <- p$data
-        output$plot <- renderPlot(p$plot, height = plot_height, width=1000) 
-    
-        ui_out <- list(list(br()))
-        ui_out <- append(ui_out, list(
-          div(id = "download-button-container",
-            downloadButton(ns("downloadPlot"), tr(rv, 'Download plot'))
-          )
-        ))
-
-        
-        ui_out <- append(ui_out, list(
-          plotOutput(ns('plot')) #%>% 
-            # tagAppendAttributes(style = "margin: 0 auto;")
-        ))
-        return(ui_out)
       }
+      # Facet if necessary and adjust arrangement, fix y-scale, get plot height for rendering
+      clean_plot_out <- clean_plot(p, facet_maa, y_scale, sel_metric, sel_maa)
+      p <- clean_plot_out$p
+      plot_height <- clean_plot_out$plot_height
+      rv$current_plot <- p$plot
+      rv$current_plot_height <- plot_height
+      rv$current_plot_data <- p$data
+      output$plot <- renderPlot(p$plot, height = plot_height, width=1000) 
+  
+      ui_out <- list(list(br()))
+      ui_out <- append(ui_out, list(
+        div(id = "download-button-container",
+          downloadButton(ns("downloadPlot"), tr(rv, 'Download plot'))
+        )
+      ))
+
+      
+      ui_out <- append(ui_out, list(
+        plotOutput(ns('plot')) #%>% 
+          # tagAppendAttributes(style = "margin: 0 auto;")
+      ))
+      return(ui_out)
     })
     
     output$downloadPlot <- downloadHandler(
